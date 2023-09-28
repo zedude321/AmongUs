ec008688dcb75f1deb92e70e313931c017d51c5c
Zedude321 <zedude321@gmail.com>
2023-09-28 21:21:39 +0800

    added animation, started work on customization

--- Assets/Code/AUPlayerController.cs
+++ Assets/Code/AUPlayerController.cs
@@ -5,14 +5,18 @@ using UnityEngine.InputSystem;
 
 public class AUPlayerController : MonoBehaviour
 {
+    [SerializeField] bool hasControl;
+    public static AU_PlayerController localPlayer;
     //Compnents
     Rigidbody myRB;
     Transform myAvatar;
+    Animator myAnim;
     //Player movement 
     [SerializeField] InputAction WASD;
     Vector2 movementInput;
     [SerializeField] float movementSpeed;
-
+    static Color myColor;
+    SpriteRenderer myAvatarSprite;
     private void OnEnable()
     {
         WASD.Enable();
@@ -25,20 +29,40 @@ public class AUPlayerController : MonoBehaviour
 
     void Start()
     {
+        if (hasControl) {
+            localPlayer = this;
+        }
         myRB = GetComponent<Rigidbody>();
         myAvatar = transform.GetChild(0);
+        myAnim = GetComponent<Animator>();
+        
+        myAvatarSprite = myAvatar.GetComponent<SpriteRenderer>();
+        if (myColor == Color.clear) {
+            myColor = Color.white;
+        }
+        myAvatarSprite.color = myColor;
     }
 
     void Update()
     {
         movementInput = WASD.ReadValue<Vector2>();
+        
         if (movementInput.x != 0)
         {
             myAvatar.localScale = new Vector2(Mathf.Sign(movementInput.x), 1);
         }
+
+        myAnim.SetFloat("Speed", movementInput.magnitude);
     }
     private void FixedUpdate()
     {
         myRB.velocity = movementInput * movementSpeed;
+    }
+
+    public void SetColor(Color newColor) {
+        myColor = newColor;
+        if (myAvatarSprite != null) {
+            myAvatarSprite.color=myColor;
+        }
     }
 }
